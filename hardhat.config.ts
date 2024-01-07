b180637f890fe1a2b22732956d652a3678be6186
Jhelison Uchoa <jhelisong@gmail.com>
2024-01-07 17:16:34 -0300

    feat: Add openzeppelin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-solhint";
+import "hardhat-gas-reporter"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
